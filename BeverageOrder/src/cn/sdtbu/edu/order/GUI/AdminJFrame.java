676988dec5a7693eabf78a5da08f9d4fd5b18cbf
Justinxiaohao <1418661311@qq.com>
2023-12-25 10:26:51 +0800

    测试提交第三次

--- BeverageOrder/src/cn/sdtbu/edu/order/GUI/AdminJFrame.java
+++ BeverageOrder/src/cn/sdtbu/edu/order/GUI/AdminJFrame.java
@@ -1,5 +1,7 @@
 package cn.sdtbu.edu.order.GUI;
 
+import cn.sdtbu.edu.order.GUI.ManageOrder.Manage;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -16,7 +18,7 @@ public class AdminJFrame extends JFrame {
 
     public AdminJFrame() {
         setTitle("奶茶管理员后台登录");
-        setBounds(700,400,280,170);
+        setBounds(800,400,270,150);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new GridLayout(3, 2));
         JLabel usernameLabel = new JLabel("                 用户名:");
@@ -30,13 +32,15 @@ public class AdminJFrame extends JFrame {
                 String username = usernameField.getText();
                 String password = new String(passwordField.getPassword());
                 if (isValidUser(username, password)) {
-                    openMainWindow();
+                    JOptionPane.showMessageDialog(null, "登录成功！");
+                    Manage ma = new Manage();
+                    ma.createAndShowGUI();
+                    setVisible(false);
                 } else {
-                    showError("用户名或密码错误");
+                    JOptionPane.showMessageDialog(null, "用户名或密码错误", "错误", JOptionPane.ERROR_MESSAGE);
                 }
             }
         });
-
         add(usernameLabel);
         add(usernameField);
         add(passwordLabel);
@@ -48,14 +52,6 @@ public class AdminJFrame extends JFrame {
 
     private boolean isValidUser(String username, String password) {
         return username.equals("zfh") && password.equals("123456");
-    }
-
-    private void openMainWindow() {
-        JOptionPane.showMessageDialog(null, "登录成功！");
-    }
-
-    private void showError(String message) {
-        JOptionPane.showMessageDialog(null, message, "错误", JOptionPane.ERROR_MESSAGE);
     }
 
     public static void main(String[] args) {
---
+++ BeverageOrder/src/cn/sdtbu/edu/order/GUI/ManageOrder/OperateSystem.java
@@ -0,0 +1,83 @@
+package cn.sdtbu.edu.order.GUI.ManageOrder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ClassName:OperateSystem
+ * Package:cn.sdtbu.edu.order.GUI.ManageOrder
+ * Author:@Wyh
+ */
+public class OperateSystem {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public static List<OperateSystem> getMealList() {
+        return mealList;
+    }
+
+    public static void setMealList(List<OperateSystem> mealList) {
+        OperateSystem.mealList = mealList;
+    }
+
+    public OperateSystem(String name, String description, double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    private String name;
+    private String description;
+    private double price;
+
+    private static List<OperateSystem> mealList = new ArrayList<>();
+
+    public static void addMeal(OperateSystem meal) {
+        mealList.add(meal);
+    }
+
+    public static void deleteMeal(String name) {
+        mealList.removeIf(meal -> meal.getName().equals(name));
+    }
+
+    public static OperateSystem findMealByName(String name) {
+        for (OperateSystem meal : mealList) {
+            if (meal.getName().equals(name)) {
+                return meal;
+            }
+        }
+        return null;
+    }
+
+    public static void updateMeal(String name, String description, double price) {
+        for (OperateSystem meal : mealList) {
+            if (meal.getName().equals(name)) {
+                meal.setDescription(description);
+                meal.setPrice(price);
+            }
+        }
+    }
+
+
+}
